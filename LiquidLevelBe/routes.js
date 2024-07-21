ad2beb6044adf6f29357543915664fafb3964cea
Михаел Йорданов <gaws@mail.bg>
2024-07-21 13:18:44 +0300

    setup routes

--- LiquidLevelBe/routes.js
+++ LiquidLevelBe/routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 const sensorsController = require("./controllers/sensorsController");
-
+//Тук казваме на рутера да използва контролера за сензорите като път за достъп до данните за сензорите в базата данни
 router.use("/sensors", sensorsController);
 
 module.exports = router;
