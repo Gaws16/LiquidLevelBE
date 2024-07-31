0b4802f439b6561d346a15c8f8f4792685f5de19
Михаел Йорданов <gaws@mail.bg>
2024-07-31 21:44:57 +0300

    add chek if env variables are correctly configured

--- LiquidLevelBe/configs/supabaseConfig.js
+++ LiquidLevelBe/configs/supabaseConfig.js
@@ -2,6 +2,13 @@ const { createClient } = require("@supabase/supabase-js");
 //Взимаме си ключа за връзка с базата данни от .env файла
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
+//Проверяваме дали са дефинирани
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error(
+    "Supabase URL и Supabase Key трябва да са дефинирани в config.env"
+  );
+}
+
 //Създаваме си клиент за връзка с базата данни
 const supabaseConfig = createClient(supabaseUrl, supabaseKey);
 //Изнасяме го, за да можем да го ползваме в други файлове
